93c483bc83b632a05f7a2380a238682eb24790c7
Avraham Nacher <nacher613@gmail.com>
2019-12-21 23:23:28 +0200

    Updates

---
+++ precache-manifest.3e05cbdb495b7b2ed71a5daba8844d99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59947002b549a713d0f5034378ac5b81",
+    "url": "/React-add-to-cart/index.html"
+  },
+  {
+    "revision": "83b353596511c9813053",
+    "url": "/React-add-to-cart/static/css/main.122df8b4.chunk.css"
+  },
+  {
+    "revision": "49c02757e0e5446b4478",
+    "url": "/React-add-to-cart/static/js/2.f7e8cde4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-add-to-cart/static/js/2.f7e8cde4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83b353596511c9813053",
+    "url": "/React-add-to-cart/static/js/main.de0bc327.chunk.js"
+  },
+  {
+    "revision": "22a642e99d1f2cf18fc7",
+    "url": "/React-add-to-cart/static/js/runtime-main.98deffb3.js"
+  }
+]);
